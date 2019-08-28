c4e394b92e181a96e28503831fbca97162995b50
Elvis Chagas <elvischagas123@gmail.com>
2019-08-28 15:39:17 -0300

    Organizando os arquivos para a instalação do webpack dev server

--- funcoes.js
+++ funcoes.js
@@ -1,3 +1,30 @@
-export function soma(a, b){
-    return a + b
+export default function div(a, b){
+    return a / b
 }
+
+export function sub(a, b){
+    return a - b
+}
+export function mult(a, b){
+    return a * b
+}
+export function rest(a, b){
+    return a % b
+}
+
+
+/*
+(main.js)
+
+//import div, { sub as subFunction } from './funcoes'
+//import possoUtilizarQualquerNome from './soma'
+import * as funcoes from './funcoes'
+
+console.log(funcoes.mult(5, 60));
+console.log(funcoes.sub(10, 8));
+console.log(funcoes.rest(23, 5));
+
+//console.log(possoUtilizarQualquerNome(1, 2));
+//console.log(subFunction(10, 2));
+//console.log(div(10, 2));
+*/
